a5693aaf514fd47838eaa1ea3343058c43f2839d
nicosommi <nicosommi@gmail.com>
2015-07-30 18:49:19 -0300

    decoupling from blunder

--- es5/spec/jsonApiFormatter.spec.js
+++ es5/spec/jsonApiFormatter.spec.js
@@ -6,19 +6,22 @@ var _libJsonApiFormatterJs = require("../lib/jsonApiFormatter.js");
 
 var _libJsonApiFormatterJs2 = _interopRequireDefault(_libJsonApiFormatterJs);
 
-var _blunder = require("blunder");
-
-var _blunder2 = _interopRequireDefault(_blunder);
-
 describe("JsonApiFormatter", function () {
 	var formatter = undefined,
 	    data = undefined,
-	    error = undefined;
+	    error = undefined,
+	    secondError = undefined,
+	    multiError = undefined;
 
 	beforeEach(function () {
 		formatter = new _libJsonApiFormatterJs2["default"]();
 		data = { id: 2, name: "Bob Belcher Jr", age: 15 };
 		error = new Error("Bob Belcher does not have any kids.");
+		secondError = new Error("No, Bob does not have a wife neither.");
+		multiError = new Error();
+		multiError.toJSON = function () {
+			return [error, secondError];
+		};
 	});
 
 	describe(".format(body)", function () {
@@ -27,10 +30,15 @@ describe("JsonApiFormatter", function () {
 		});
 
 		it("add the .errors root element on the provided body if the (body instanceof MultiError === true)", function () {
-			formatter.format(new _blunder2["default"](error)).should.eql({ errors: [{
+			formatter.format(multiError).should.eql({
+				errors: [{
 					title: error.name,
 					details: error.message
-				}] });
+				}, {
+					title: secondError.name,
+					details: secondError.message
+				}]
+			});
 		});
 
 		it("add the .errors root element on the provided body if the (body instanceof Error === true)", function () {
